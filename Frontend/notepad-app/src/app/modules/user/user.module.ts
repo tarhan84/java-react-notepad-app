ca261f946ae0f905b94a26d583703df9bd48ae4e
serhat.tarhan <serhat.tarhan@paycore.com>
2022-04-15 22:43:09 +0300

    fix

--- Frontend/notepad-app/src/app/modules/user/user.module.ts
+++ Frontend/notepad-app/src/app/modules/user/user.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
@@ -9,7 +11,10 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    HttpClientModule
+
   ]
 })
 export class UserModule { }
